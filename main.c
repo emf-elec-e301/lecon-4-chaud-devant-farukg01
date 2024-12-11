05ada4a82d746b9f32a76be34ad281c946b36886
farukg01 <faruk.guelal@studentfr.ch>
2024-12-11 08:43:10 +0100

    chaud devant

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 //----------------------------------------------------------
 // MAIN APPLICATION
 //----------------------------------------------------------
-int main(void)
+int main()
 {
     //----------------------------------------------------------
     // initialisation du microcontroleur
@@ -24,15 +24,39 @@ int main(void)
     //----------------------------------------------------------
     // VOTRE CODE CI-DESSOUS
     // INSTRUCTIONS EXECUTEES UNE SEULE FOIS AU DEMARRAGE
+    
+    uint8_t distance = ultrason_distance();
     //----------------------------------------------------------
     
     while(1){
         //----------------------------------------------------------
         // VOTRE CODE CI-DESSOUS
+        
+        ultrason_distance();
+        
+        if(distance >= 1 && distance < 3){
+            buzzer_tone(NOTE_C1, 500, 100);
+        }
+        
+        else if(distance >= 3 && distance < 5){
+            buzzer_tone(NOTE_C2,500,100);
+        }
+        
+        else if(distance >= 5 && distance < 10){
+            buzzer_tone(NOTE_C3,500,100);
+        }
+        
+        else{
+            buzzer_tone(NOTE_C7, 500, 100);
+        }
+        
+        
+        
         // INSTRUCTIONS EXECUTEES EN BOUCLE INDEFINIMENT
         //----------------------------------------------------------
         
     }
+ 
 }
 //----------------------------------------------------------
 // FIN DU CODE
